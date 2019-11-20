7a25353994ab6e552a18bcf000453f58749c7831
rico20200 <rico20200@hotmail.com>
2019-11-20 16:08:11 -0200

    criacao da funcao main

--- program.c
+++ program.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <gtk/gtk.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
+    
     getchar();
+
+    GtkWidget *janela;
+
+    gtk_init(&argc, &argv);
+
+    janela = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_position(GTK_WINDOW(janela), GTK_WIN_POS_CENTER);
+    
+    g_signal_connect(G_OBJECT(janela), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(janela);
+
+    gtk_main();
+
     return 0;
 }
